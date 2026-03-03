e6aa66a72007e752728a4a0352104b17e9b6ef10
Farmers-j-vishnukrishnamoort <82074345+Farmers-j-vishnukrishnamoort@users.noreply.github.com>
2026-03-03 09:06:40 +0000

    Fourth commit

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,9 @@ SECRET_KEY = 'django-insecure-y$e^zr5vq7e1xs!^5g*0ybrzo5g1=#jd)uvr-7%e-7pp#8(=k=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# Allow all hosts and codespace URL
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', 'localhost')
+ALLOWED_HOSTS = ['*', f'{CODESPACE_NAME}-8000.app.github.dev', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -136,8 +139,6 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = ['*']
 CORS_ALLOW_METHODS = ['*']
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
